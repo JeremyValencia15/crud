bba67ebc09916a6539e45eed46e38f6a94e6173f
JeremyValencia15 <jeremyvama10@gmail.com>
2025-08-10 20:50:12 -0600

    Añadimos JWT, y configuramos lo que necesitamos

--- src/main/java/com/jeremy/curso/springboot/crud/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/jeremy/curso/springboot/crud/security/filter/JwtAuthenticationFilter.java
@@ -1,18 +1,25 @@
 package com.jeremy.curso.springboot.crud.security.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jeremy.curso.springboot.crud.entities.User;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -20,11 +27,15 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     private AuthenticationManager authenticationManager;
 
+    // NEcesitamos una llave para los tokens, se genera de la siguiente manera
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
 
-    //Este se usa para la autenticacion, aqui llega el username, y password, se valida el token y se corrobora que todo este bienvv   
+    // Este se usa para la autenticacion, aqui llega el username, y password, se
+    // valida el token y se corrobora que todo este bienvv
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
@@ -45,9 +56,36 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             e.printStackTrace();
         }
 
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                password);
 
         return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+
+        // En el authresult viaja cierta informacionm aquella que pasamos con el filter,
+        // por
+        // lo tanto el username se encuentra aqui
+        User user = (User) authResult.getPrincipal();
+        String username = user.getUsername();
+
+        String token = Jwts.builder().subject(username).signWith(SECRET_KEY).compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+        //DE esta manera lo convertimos en JSON
+        Map<String, String> body = new  HashMap<>();
+        body.put("token", token);
+        body.put("username", username);
+        body.put("message", String.format("Hola %s has iniciado sesión con exito", username));
+
+        //Aqui generamos el json
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("application/json");
+        response.setStatus(200);
     }
 
 }
